745e65a9068da648f66aefe55fb2a9f99a6d8fa4
Masha <fppl0101@gmail.com>
2023-05-28 12:40:13 +0300

    Add win/lose logic

--- src/main.rs
+++ src/main.rs
@@ -73,6 +73,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         if invaders.update(delta) {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
 
         let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
         for drawable in drawables {
@@ -80,6 +83,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop;
+        }
+
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'gameloop;
+        }
     }
 
     drop(render_tx);
